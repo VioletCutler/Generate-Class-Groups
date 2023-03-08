314c89d6f2bdacce17d6f1615a73af37ba848ec3
violetcutler <luminancesignal@gmail.com>
2023-03-08 13:45:15 -0500

    rendering student list and can add students to list of absent students

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,9 +3,38 @@ import { studentList } from "../helperFunctions/dummyData"
 
 const Homepage = () => {
     const [classList, setClassList] = useState(studentList)
-    console.log(classList)
+    const [absentStudents, setAbsentStudents] = useState([])
+
+    function handleAbsentStudentClick(e){
+        console.log(e.target.id)
+        console.log(classList[e.target.id])
+        setAbsentStudents([...absentStudents, classList[e.target.id]])
+        const updatedClassList = classList.splice(e.target.id, 1)
+    }
+
     return (
-        <h2>This is the Homepage</h2>
+        <div>
+        <h2>Generate Groups Below</h2>
+        <div>      
+             <h3>Absent Students</h3> 
+            {absentStudents.length ? 
+     absentStudents.map((absentStudent, idx) => {
+                return (
+                    <p key={idx}>{absentStudent}</p>
+                )
+            }) : null}
+        </div>
+        <div>
+            {classList.length ? classList.map((student, idx) => {
+                return(
+                    <div className="current-class-list" key={idx}>
+                    <p>{student}</p>
+                    <button id={idx} onClick={handleAbsentStudentClick}>Absent</button>
+                    </div>
+                )
+            }) : <p>No Students Yet</p>}
+        </div>
+        </div>
     )
 }
 
