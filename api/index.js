478d09a0f7dbbe560b150bd66f4b07f97199eedc
Violet Cutler <91086765+VioletCutler@users.noreply.github.com>
2023-03-27 15:29:29 -0400

    added API

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const Express = require('express');
+const router = Express.Router();
+const { client } = require('../db/client')
+router.use('*', (req, res, next)=> {
+    console.log('a request is being made to the API router')
+    next()
+})
+
+router.use('/health', async (req, res, next)=>{
+    try{
+        const uptime = process.uptime();
+        // const {rows: [dbConnection]} = await client.query(`SELECT NOW();`)
+        console.log('line 14')
+        const currentTime = new Date()
+        const lastRestart = new Intl.DateTimeFormat('en', {timestyle: 'long', dateStyle: 'long', timeZone: 'America/New_York'}).format(currentTime - (uptime * 1000))
+        res.send({message: 'healthy', uptime, currentTime, lastRestart})
+    }catch(error){
+        next(error)
+    }
+})
+
+router.use(async(req, res, next)=> {
+    console.log('req middleware')
+    res.send('Authorization')
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const { client } = require("./db/client");
+const morgan = require('morgan')
 const Express = require("express");
 const app = Express();
 const PORT = 8080
 
-app.use((req, res, next) => {
-  console.log("request made to the server");
-  next();
-});
+app.use(morgan('dev'))
+app.use(Express.json())
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
+app.use((req, res, next)=>{
+    console.log('<___ Body Logger Start ___>')
+    console.log(req.body)
+    console.log('<___ Body Logger End ___')
+    next()
 })
+
+const apiRouter = require('./api')
+app.use('/api', apiRouter)
+
+
+app.get('*', (req, res, next)=>{  
+    res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'})
+})
+
+app.use((error, req, res, next)=> {
+    console.log('Error', error)
+    res.send({error: error.message, name: error.name, message: error.message, table: error.table})
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is listening on PORT ${PORT}`)
 })
